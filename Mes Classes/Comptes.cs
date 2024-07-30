7af82d238861a5e1d5df7a0654e9cebf2a80df49
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 11:56:57 +0100

    update

--- Mes Classes/Comptes.cs
+++ Mes Classes/Comptes.cs
@@ -21,6 +21,42 @@ namespace Gestion_Compte_Clients.Mes_Classes
             ClientID = clientID;
             Solde = solde;
         }
+
+        private DataAccess data = new DataAccess();
+
+        public int EnregistrerTransaction(Comptes comptes)
+        {
+            int resultat = 0;
+            string strQuery = "INSERT INTO Comptes ( ClientID, Solde) VALUES ( @ClientID, @Solde)";
+
+            if (data.OpenConnection())
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand(strQuery, data.conn))
+                    {
+                        cmd.Parameters.AddWithValue("@ClientID", comptes.ClientID);
+                        cmd.Parameters.AddWithValue("@Solde", comptes.Solde);
+
+                        resultat = cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erreur lors de la creation du compte : {ex.Message}");
+                }
+                finally
+                {
+                    data.CloseConnection();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Impossible d'ouvrir la connexion à la base de données.");
+            }
+
+            return resultat;
+        }
     }
 
 }
