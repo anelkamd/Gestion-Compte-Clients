fc6cb351b2ed59b53a324798c63f308bc07e1376
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 15:38:11 +0100

    update

--- frmTransactions.Designer.cs
+++ frmTransactions.Designer.cs
@@ -173,7 +173,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(524, 16);
+            pictureBox1.Location = new Point(524, 18);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(274, 255);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
