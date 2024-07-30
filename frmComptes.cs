697d5764966ab1505801cc7749e4a9716084a9d4
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 12:55:48 +0100

    update

--- frmComptes.cs
+++ frmComptes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gestion_Compte_Clients.Mes_Classes;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,38 @@ namespace Gestion_Compte_Clients
         public frmComptes()
         {
             InitializeComponent();
+        }
+
+
+        private int EnregistrerTransaction(Comptes comptes)
+        {
+            string connectionString = "Data Source=ANELKA-MD; Initial Catalog=Gestion_Compte_Clients; User ID=Anelka; Password=26355217; Encrypt=false";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Compte (Solde, DateCreation) VALUES (@Solde, @DateCreation)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Solde", comptes.Solde);
+                        command.Parameters.AddWithValue("@DateCreation", comptes.DateCreation);
+
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show("Erreur SQL: " + sqlEx.Message);
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur: " + ex.Message);
+                return -1;
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
