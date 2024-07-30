5920b71d0171cf5ebd5d3dbf407f79e42e352e2c
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 08:25:44 +0100

    update

---
+++ frmTransactions.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Gestion_Compte_Clients
+{
+    partial class frmTransactions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(28, 113);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Montant :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(143, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(329, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(374, 175);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(98, 32);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Retrait";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox2.Location = new Point(259, 175);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(91, 32);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "Depot";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Poppins Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(28, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(226, 31);
+            label2.TabIndex = 4;
+            label2.Text = "Type de transaction :";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(177, 227);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(295, 31);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Poppins Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(28, 232);
+            label3.Name = "label3";
+            label3.Size = new Size(143, 31);
+            label3.TabIndex = 6;
+            label3.Text = "Date & Heure :";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(28, 280);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.TabIndex = 7;
+            // 
+            // frmTransactions
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 544);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label2);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "frmTransactions";
+            Text = "frmTransactions";
+            Load += frmTransactions_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ frmTransactions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_Compte_Clients
+{
+    public partial class frmTransactions : Form
+    {
+        public frmTransactions()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmTransactions_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
