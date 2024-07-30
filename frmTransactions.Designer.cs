8809bb21775765d29b7992be142033bacf843bb7
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 08:30:40 +0100

    update

--- frmTransactions.Designer.cs
+++ frmTransactions.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
+            txtMontant = new TextBox();
+            chkRetrait = new CheckBox();
+            chkDepot = new CheckBox();
             label2 = new Label();
-            dateTimePicker1 = new DateTimePicker();
+            dtTransaction = new DateTimePicker();
             label3 = new Label();
             dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -49,35 +51,35 @@
             label1.TabIndex = 0;
             label1.Text = "Montant :";
             // 
-            // textBox1
+            // txtMontant
             // 
-            textBox1.Location = new Point(143, 113);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(329, 31);
-            textBox1.TabIndex = 1;
+            txtMontant.Location = new Point(143, 113);
+            txtMontant.Name = "txtMontant";
+            txtMontant.Size = new Size(329, 31);
+            txtMontant.TabIndex = 1;
             // 
-            // checkBox1
+            // chkRetrait
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            checkBox1.Location = new Point(374, 175);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(98, 32);
-            checkBox1.TabIndex = 2;
-            checkBox1.Text = "Retrait";
-            checkBox1.UseVisualStyleBackColor = true;
+            chkRetrait.AutoSize = true;
+            chkRetrait.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkRetrait.Location = new Point(374, 175);
+            chkRetrait.Name = "chkRetrait";
+            chkRetrait.Size = new Size(98, 32);
+            chkRetrait.TabIndex = 2;
+            chkRetrait.Text = "Retrait";
+            chkRetrait.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // chkDepot
             // 
-            checkBox2.AutoSize = true;
-            checkBox2.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            checkBox2.Location = new Point(259, 175);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(91, 32);
-            checkBox2.TabIndex = 3;
-            checkBox2.Text = "Depot";
-            checkBox2.UseVisualStyleBackColor = true;
-            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            chkDepot.AutoSize = true;
+            chkDepot.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkDepot.Location = new Point(259, 175);
+            chkDepot.Name = "chkDepot";
+            chkDepot.Size = new Size(91, 32);
+            chkDepot.TabIndex = 3;
+            chkDepot.Text = "Depot";
+            chkDepot.UseVisualStyleBackColor = true;
+            chkDepot.CheckedChanged += checkBox2_CheckedChanged;
             // 
             // label2
             // 
@@ -89,12 +91,12 @@
             label2.TabIndex = 4;
             label2.Text = "Type de transaction :";
             // 
-            // dateTimePicker1
+            // dtTransaction
             // 
-            dateTimePicker1.Location = new Point(177, 227);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(295, 31);
-            dateTimePicker1.TabIndex = 5;
+            dtTransaction.Location = new Point(177, 227);
+            dtTransaction.Name = "dtTransaction";
+            dtTransaction.Size = new Size(295, 31);
+            dtTransaction.TabIndex = 5;
             // 
             // label3
             // 
@@ -112,21 +114,47 @@
             dataGridView1.Location = new Point(28, 280);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.Size = new Size(736, 225);
             dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(161, 526);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 34);
+            button2.TabIndex = 15;
+            button2.Text = "Annuler";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DeepSkyBlue;
+            button1.Font = new Font("Poppins Medium", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(26, 526);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 34);
+            button1.TabIndex = 14;
+            button1.Text = "Valider";
+            button1.UseVisualStyleBackColor = false;
             // 
             // frmTransactions
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(989, 544);
+            ClientSize = new Size(799, 572);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
             Controls.Add(label3);
-            Controls.Add(dateTimePicker1);
+            Controls.Add(dtTransaction);
             Controls.Add(label2);
-            Controls.Add(checkBox2);
-            Controls.Add(checkBox1);
-            Controls.Add(textBox1);
+            Controls.Add(chkDepot);
+            Controls.Add(chkRetrait);
+            Controls.Add(txtMontant);
             Controls.Add(label1);
             Name = "frmTransactions";
             Text = "frmTransactions";
@@ -139,12 +167,14 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
+        private TextBox txtMontant;
+        private CheckBox chkRetrait;
+        private CheckBox chkDepot;
         private Label label2;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dtTransaction;
         private Label label3;
         private DataGridView dataGridView1;
+        private Button button2;
+        private Button button1;
     }
 }
