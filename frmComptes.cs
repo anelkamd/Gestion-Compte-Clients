846e5be493b9a9901c619ed2d330fe4225999acc
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-30 13:43:40 +0100

    updtae

--- frmComptes.cs
+++ frmComptes.cs
@@ -1,14 +1,5 @@
 ﻿using Gestion_Compte_Clients.Mes_Classes;
 using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Gestion_Compte_Clients
 {
