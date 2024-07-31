4080fd2bd093ea6ea48725b3a6786d34b2c89dbf
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-07-31 08:54:04 +0100

    update

--- frmTransactions.cs
+++ frmTransactions.cs
@@ -65,7 +65,7 @@ namespace Gestion_Compte_Clients
                 // Vérifier que le type de transaction est valide
                 if (transaction.TypeTransaction != "Depot" && transaction.TypeTransaction != "Retrait")
                 {
-                    throw new ArgumentException("Le Type de Transaction doit etre cocher apre avoir remplie le formulaire");
+                    throw new ArgumentException("Le Type de Transaction doit etre cocher apres avoir remplie le formulaire");
                 }
 
                 using (SqlConnection connection = new SqlConnection(connectionString))
